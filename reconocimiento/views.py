e3de50cc0a5ed4fb3c0a3fb83278e997d635ced3
NiicolasTorres <federico.-torres@hotmail.com>
2025-04-18 18:51:04 -0300

    cambios de ip

--- reconocimiento/views.py
+++ reconocimiento/views.py
@@ -73,11 +73,10 @@ def home(request):
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(0)
     try:
-        s.connect(('10.254.254.254', 1))
+        s.connect(('8.8.8.8', 80))
         local_ip = s.getsockname()[0]
-    except:
+    except Exception:
         local_ip = '127.0.0.1'
     finally:
         s.close()
